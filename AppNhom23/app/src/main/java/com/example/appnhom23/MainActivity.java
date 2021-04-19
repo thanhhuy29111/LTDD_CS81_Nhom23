8a541fd1f0c29c5f010defdd81bf9d727682e19f
unknown <ngoc hau>
2021-04-19 18:27:25 +0700

    them giao dien

--- AppNhom23/app/src/main/java/com/example/appnhom23/MainActivity.java
+++ AppNhom23/app/src/main/java/com/example/appnhom23/MainActivity.java
@@ -14,28 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // dm
         ListView listView;
         ArrayList<DanhMuc> arrayList;
         AdapterDanhMuc adapter;
+        // mon an
+        ListView listViewMA;
+        ArrayList<MonAn> arrayListMA;
+        AdapterDanhMuc adapterMA;
 
+        // dm
         listView = findViewById(R.id.listviewDM);
         arrayList = new ArrayList<>();
 
+        // mon an
+        listViewMA = findViewById(R.id.listviewMonAn);
+        arrayListMA = new ArrayList<>();
+
+        //dm
         arrayList.add(new DanhMuc("MÓN XÀO",R.drawable.monxao));
-        arrayList.add(new DanhMuc("Món chiên",R.drawable.monchien));
-        arrayList.add(new DanhMuc("Món chiên",R.drawable.kho));
-        arrayList.add(new DanhMuc("Món chiên",R.drawable.monchay));
-        arrayList.add(new DanhMuc("Món chiên",R.drawable.montrangmieng));
-        arrayList.add(new DanhMuc("Món chiên",R.drawable.montrangmieng));
-        arrayList.add(new DanhMuc("Món chiên",R.drawable.montrangmieng));
-        arrayList.add(new DanhMuc("Món chiên",R.drawable.montrangmieng));
-        arrayList.add(new DanhMuc("Món chiên",R.drawable.montrangmieng));
-        arrayList.add(new DanhMuc("Món chiên",R.drawable.montrangmieng));
-        arrayList.add(new DanhMuc("Món chiên",R.drawable.montrangmieng));
-        arrayList.add(new DanhMuc("Món chiên",R.drawable.montrangmieng));
-        arrayList.add(new DanhMuc("Món chiên",R.drawable.montrangmieng));
+        arrayList.add(new DanhMuc("MÓN CHIÊN",R.drawable.monchien));
+        arrayList.add(new DanhMuc("MÓN KHO",R.drawable.kho));
+        arrayList.add(new DanhMuc("MÓN CHAY",R.drawable.monchay));
+        arrayList.add(new DanhMuc("MÓN TRÁNG MIỆNG",R.drawable.montrangmieng));
         adapter = new AdapterDanhMuc(MainActivity.this,R.layout.layout_danhmuc,arrayList);
         listView.setAdapter(adapter);
+
+        //mon an
     }
 }
